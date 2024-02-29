55eaa0625c6c113cb1015c1a8355d54a7c053cae
PatrickLaabs <patrick.laabs@me.com>
2024-02-29 23:52:12 +0100

    updating vars

---
+++ cmd/frigg/bootstrap/capd/cluster/vars.go
@@ -0,0 +1,6 @@
+package cluster
+
+var FriggDirName = ".frigg"
+var BootstrapkubeconfigName = "bootstrapcluster.kubeconfig"
+var ManagementKubeconfigName = "argohubmgmtcluster.kubeconfig"
+var WorkloadKubeconfigName = "workloadcluster.kubeconfig"
