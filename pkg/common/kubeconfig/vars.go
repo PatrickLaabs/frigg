082d2126acacbf706de4a78ccac7716b4ff1f8cb
PatrickLaabs <patrick.laabs@me.com>
2024-03-03 10:29:04 +0100

    deleted unneeded vars.go file

--- pkg/common/kubeconfig/vars.go
+++
@@ -1,6 +0,0 @@
-package kubeconfig
-
-var friggDirName = ".frigg"
-var bootstrapkubeconfigName = "bootstrapcluster.kubeconfig"
-var managementKubeconfigName = "argohubmgmtcluster.kubeconfig"
-var workloadKubeconfigName = "workloadcluster.kubeconfig"
